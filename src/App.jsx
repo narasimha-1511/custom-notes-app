cf78d60d647851bef23008fb1ebe82fc69e5cec3
narasimha0503 <narasimha@rankz.io>
2024-08-20 01:38:34 +0530

    creating a login page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./pages/login";
+
+function App() {
+  return (
+    <div className="h-full w-full ">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center px-16  bg-white"></div>
+  );
+};
+
+export default Login;
